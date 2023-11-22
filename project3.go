c8116c4278e7410424ca519a8c6e4bd6f9b06bb3
juovani <80779885+juovani@users.noreply.github.com>
2023-11-22 12:32:55 -0500

    bullets working

--- project3.go
+++ project3.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/lafriks/go-tiled"
 	"golang.org/x/image/colornames"
@@ -59,23 +60,45 @@ const (
 	LEFT
 	RIGHT
 )
+const (
+	COIN_FRAME_WIDTH     = 27
+	COIN_HEIGHT          = 27
+	COIN_FRAME_COUNT     = 1
+	COIN_FRAME_PER_SHEET = 8
+)
 
 type AnimatedSpriteDemo3 struct {
 	spriteSheet *ebiten.Image
+	bullet      *ebiten.Image
 	playerXLoc  int
 	playerYLoc  int
 	direction   int
 	frame       int
 	frameDelay  int
 	damage      int
+	temp        bool
 	level       *tiled.Map
 	levels      int
 	tileHash    map[uint32]*ebiten.Image
-	enemy       enemy
+	enemy1      enemy
 	enemy2      enemy
+	enemy3      enemy
 	npc1        npc
+	shot        []shots
 	msg         bool
 	textFont    font.Face
+	coin        coins
+}
+type coins struct {
+	sprite   *ebiten.Image
+	coinXLoc int
+	coinYLoc int
+}
+type shots struct {
+	sprite     *ebiten.Image
+	direction  int
+	bulletXLoc int
+	bulletYLoc int
 }
 type enemy struct {
 	sprite     *ebiten.Image
@@ -96,6 +119,11 @@ type npc struct {
 	direction  int
 }
 
+func newShots(image *ebiten.Image) shots {
+	return shots{
+		sprite: image,
+	}
+}
 func (demoGame *AnimatedSpriteDemo3) Update() error {
 	demoGame.frameDelay += 1
 	if demoGame.frameDelay%FRAME_COUNT == 0 {
@@ -119,22 +147,43 @@ func (demoGame *AnimatedSpriteDemo3) Update() error {
 			demoGame.frame = 0
 		}
 	}
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		demoGame.temp = true
 
-	demoGame.enemy.frameDelay += 1
-	if demoGame.enemy.frameDelay%ENEMY_FRAME_COUNT == 0 {
-		demoGame.enemy.frame += 1
-		if demoGame.enemy.frame >= ENEMY_FRAME_PER_SHEET {
-			demoGame.enemy.frame = 0
-		}
-		if demoGame.enemy.direction == ENEMY_RIGHT {
-			demoGame.enemy.xLocNnemy += 3
-			if demoGame.enemy.xLocNnemy >= 350 {
-				demoGame.enemy.direction = ENEMY_LEFT
+		newBullet := newShots(demoGame.bullet)
+		newBullet.bulletXLoc = demoGame.playerXLoc
+		newBullet.bulletYLoc = demoGame.playerYLoc
+		newBullet.direction = demoGame.direction
+		demoGame.shot = append(demoGame.shot, newBullet)
+	} else {
+		for i := range demoGame.shot {
+			if demoGame.shot[i].direction == RIGHT {
+				demoGame.shot[i].bulletXLoc += 4
+			} else if demoGame.shot[i].direction == LEFT {
+				demoGame.shot[i].bulletXLoc -= 4
+			} else if demoGame.shot[i].direction == UP {
+				demoGame.shot[i].bulletYLoc -= 4
+			} else if demoGame.shot[i].direction == DOWN {
+				demoGame.shot[i].bulletYLoc += 4
 			}
-		} else if demoGame.enemy.direction == ENEMY_LEFT {
-			demoGame.enemy.xLocNnemy -= 3
-			if demoGame.enemy.xLocNnemy <= 200 {
-				demoGame.enemy.direction = ENEMY_RIGHT
+		}
+	}
+
+	demoGame.enemy1.frameDelay += 1
+	if demoGame.enemy1.frameDelay%ENEMY_FRAME_COUNT == 0 {
+		demoGame.enemy1.frame += 1
+		if demoGame.enemy1.frame >= ENEMY_FRAME_PER_SHEET {
+			demoGame.enemy1.frame = 0
+		}
+		if demoGame.enemy1.direction == ENEMY_RIGHT {
+			demoGame.enemy1.xLocNnemy += 3
+			if demoGame.enemy1.xLocNnemy >= 350 {
+				demoGame.enemy1.direction = ENEMY_LEFT
+			}
+		} else if demoGame.enemy1.direction == ENEMY_LEFT {
+			demoGame.enemy1.xLocNnemy -= 3
+			if demoGame.enemy1.xLocNnemy <= 200 {
+				demoGame.enemy1.direction = ENEMY_RIGHT
 			}
 		}
 	}
@@ -212,13 +261,23 @@ func (demoGame *AnimatedSpriteDemo3) Update() error {
 	} else {
 		demoGame.msg = false
 	}
-
-	enemy1 := image.Rect(demoGame.enemy.xLocNnemy, demoGame.enemy.yLocEnemy, demoGame.enemy.xLocNnemy+ENEMY_FRAME_WIDTH, demoGame.enemy.yLocEnemy+ENEMY_HEIGHT)
-	if playerChar.Overlaps(enemy1) {
-		demoGame.enemy.alive = false
+	if demoGame.levels == 1 {
+		enemy1 := image.Rect(demoGame.enemy1.xLocNnemy, demoGame.enemy1.yLocEnemy, demoGame.enemy1.xLocNnemy+ENEMY_FRAME_WIDTH, demoGame.enemy1.yLocEnemy+ENEMY_HEIGHT)
+		if playerChar.Overlaps(enemy1) {
+			demoGame.enemy1.alive = false
+		}
+		enemy2 := image.Rect(demoGame.enemy2.xLocNnemy, demoGame.enemy2.yLocEnemy, demoGame.enemy2.xLocNnemy+ENEMY_FRAME_WIDTH, demoGame.enemy2.yLocEnemy+ENEMY_HEIGHT)
+		if playerChar.Overlaps(enemy2) {
+			demoGame.enemy2.alive = false
+		}
 	}
 
-	// tiles collision
+	//enemy3 := image.Rect(demoGame.enemy3.xLocNnemy, demoGame.enemy3.yLocEnemy, demoGame.enemy3.xLocNnemy+ENEMY_FRAME_WIDTH, demoGame.enemy3.yLocEnemy+ENEMY_HEIGHT)
+	//if playerChar.Overlaps(enemy3) {
+	//	demoGame.enemy3.alive = false
+	//}
+
+	// tiles collision for level 0
 	if demoGame.levels == 0 {
 		for tileY := 0; tileY < demoGame.level.Height; tileY += 1 {
 			for tileX := 0; tileX < demoGame.level.Width; tileX += 1 {
@@ -240,6 +299,7 @@ func (demoGame *AnimatedSpriteDemo3) Update() error {
 			}
 		}
 	} else if demoGame.levels == 1 || demoGame.levels == 2 {
+		// collision for level 1 and 2
 		for tileY := 0; tileY < demoGame.level.Height; tileY += 1 {
 			for tileX := 0; tileX < demoGame.level.Width; tileX += 1 {
 				tileID := demoGame.level.Layers[1].Tiles[tileY*demoGame.level.Width+tileX].ID
@@ -266,6 +326,7 @@ func (demoGame *AnimatedSpriteDemo3) Update() error {
 
 func (demoGame AnimatedSpriteDemo3) Draw(screen *ebiten.Image) {
 	drawOptions := ebiten.DrawImageOptions{}
+	// drawing all the layers of the map
 	for _, layer := range demoGame.level.Layers {
 		for tileY := 0; tileY < demoGame.level.Height; tileY++ {
 			for tileX := 0; tileX < demoGame.level.Width; tileX++ {
@@ -287,6 +348,55 @@ func (demoGame AnimatedSpriteDemo3) Draw(screen *ebiten.Image) {
 			}
 		}
 	}
+	// drawing the player
+	drawOptions.GeoM.Reset()
+	drawOptions.GeoM.Translate(float64(demoGame.playerXLoc), float64(demoGame.playerYLoc))
+	screen.DrawImage(demoGame.spriteSheet.SubImage(image.Rect(demoGame.frame*GUY_FRAME_WIDTH,
+		demoGame.direction*GUY_HEIGHT,
+		demoGame.frame*GUY_FRAME_WIDTH+GUY_FRAME_WIDTH,
+		demoGame.direction*GUY_HEIGHT+GUY_HEIGHT)).(*ebiten.Image), &drawOptions)
+	// drawing NPC1 in level 0
+	if demoGame.levels == 0 {
+		drawOptions.GeoM.Reset()
+		drawOptions.GeoM.Translate(float64(demoGame.npc1.xLocNPC), float64(demoGame.npc1.yLocNPC))
+		screen.DrawImage(demoGame.npc1.sprite.SubImage(image.Rect(demoGame.npc1.frame*NPC_FRAME_WIDTH,
+			demoGame.npc1.direction*NPC_HEIGHT,
+			demoGame.npc1.frame*NPC_FRAME_WIDTH+NPC_FRAME_WIDTH,
+			demoGame.npc1.direction*NPC_HEIGHT+NPC_HEIGHT)).(*ebiten.Image), &drawOptions)
+	}
+	// drawing the enemies in level 1
+	if demoGame.levels == 1 {
+		if demoGame.enemy1.alive == true {
+			drawOptions.GeoM.Reset()
+			drawOptions.GeoM.Translate(float64(demoGame.enemy1.xLocNnemy), float64(demoGame.enemy1.yLocEnemy))
+			screen.DrawImage(demoGame.enemy1.sprite.SubImage(image.Rect(demoGame.enemy1.frame*ENEMY_FRAME_WIDTH,
+				demoGame.enemy1.direction*ENEMY_HEIGHT,
+				demoGame.enemy1.frame*ENEMY_FRAME_WIDTH+ENEMY_FRAME_WIDTH,
+				demoGame.enemy1.direction*ENEMY_HEIGHT+ENEMY_HEIGHT)).(*ebiten.Image), &drawOptions)
+		} else {
+			demoGame.damage++
+		}
+		if demoGame.enemy2.alive == true {
+			drawOptions.GeoM.Reset()
+			drawOptions.GeoM.Translate(float64(demoGame.enemy2.xLocNnemy), float64(demoGame.enemy2.yLocEnemy))
+			screen.DrawImage(demoGame.enemy2.sprite.SubImage(image.Rect(demoGame.enemy2.frame*ENEMY_FRAME_WIDTH,
+				demoGame.enemy2.direction*ENEMY_HEIGHT,
+				demoGame.enemy2.frame*ENEMY_FRAME_WIDTH+ENEMY_FRAME_WIDTH,
+				demoGame.enemy2.direction*ENEMY_HEIGHT+ENEMY_HEIGHT)).(*ebiten.Image), &drawOptions)
+		} else {
+			demoGame.damage++
+		}
+		//if demoGame.enemy3.alive == true {
+		//	drawOptions.GeoM.Reset()
+		//	drawOptions.GeoM.Translate(float64(demoGame.enemy3.xLocNnemy), float64(demoGame.enemy3.yLocEnemy))
+		//	screen.DrawImage(demoGame.enemy3.sprite.SubImage(image.Rect(demoGame.enemy3.frame*ENEMY_FRAME_WIDTH,
+		//		demoGame.enemy3.direction*ENEMY_HEIGHT,
+		//		demoGame.enemy3.frame*ENEMY_FRAME_WIDTH+ENEMY_FRAME_WIDTH,
+		//		demoGame.enemy3.direction*ENEMY_HEIGHT+ENEMY_HEIGHT)).(*ebiten.Image), &drawOptions)
+		//} else {
+		//	demoGame.damage++
+		//}
+	}
 
 	DrawCenteredText(screen, demoGame.textFont, fmt.Sprintf("Damage: %d", demoGame.damage), 65, 30)
 	if demoGame.msg == true && demoGame.levels == 0 {
@@ -296,38 +406,11 @@ func (demoGame AnimatedSpriteDemo3) Draw(screen *ebiten.Image) {
 			"them. they are trying to take over"), 400, 200)
 	}
 
-	drawOptions.GeoM.Reset()
-	drawOptions.GeoM.Translate(float64(demoGame.playerXLoc), float64(demoGame.playerYLoc))
-	screen.DrawImage(demoGame.spriteSheet.SubImage(image.Rect(demoGame.frame*GUY_FRAME_WIDTH,
-		demoGame.direction*GUY_HEIGHT,
-		demoGame.frame*GUY_FRAME_WIDTH+GUY_FRAME_WIDTH,
-		demoGame.direction*GUY_HEIGHT+GUY_HEIGHT)).(*ebiten.Image), &drawOptions)
-
-	if demoGame.levels == 0 {
-		drawOptions.GeoM.Reset()
-		drawOptions.GeoM.Translate(float64(demoGame.npc1.xLocNPC), float64(demoGame.npc1.yLocNPC))
-		screen.DrawImage(demoGame.npc1.sprite.SubImage(image.Rect(demoGame.npc1.frame*NPC_FRAME_WIDTH,
-			demoGame.npc1.direction*NPC_HEIGHT,
-			demoGame.npc1.frame*NPC_FRAME_WIDTH+NPC_FRAME_WIDTH,
-			demoGame.npc1.direction*NPC_HEIGHT+NPC_HEIGHT)).(*ebiten.Image), &drawOptions)
-	}
-
-	if demoGame.levels == 1 {
-		if demoGame.enemy.alive == true {
+	if demoGame.temp == true {
+		for _, shot := range demoGame.shot {
 			drawOptions.GeoM.Reset()
-			drawOptions.GeoM.Translate(float64(demoGame.enemy.xLocNnemy), float64(demoGame.enemy.yLocEnemy))
-			screen.DrawImage(demoGame.enemy.sprite.SubImage(image.Rect(demoGame.enemy.frame*ENEMY_FRAME_WIDTH,
-				demoGame.enemy.direction*ENEMY_HEIGHT,
-				demoGame.enemy.frame*ENEMY_FRAME_WIDTH+ENEMY_FRAME_WIDTH,
-				demoGame.enemy.direction*ENEMY_HEIGHT+ENEMY_HEIGHT)).(*ebiten.Image), &drawOptions)
-		}
-		if demoGame.enemy2.alive == true {
-			drawOptions.GeoM.Reset()
-			drawOptions.GeoM.Translate(float64(demoGame.enemy2.xLocNnemy), float64(demoGame.enemy2.yLocEnemy))
-			screen.DrawImage(demoGame.enemy2.sprite.SubImage(image.Rect(demoGame.enemy2.frame*ENEMY_FRAME_WIDTH,
-				demoGame.enemy2.direction*ENEMY_HEIGHT,
-				demoGame.enemy2.frame*ENEMY_FRAME_WIDTH+ENEMY_FRAME_WIDTH,
-				demoGame.enemy2.direction*ENEMY_HEIGHT+ENEMY_HEIGHT)).(*ebiten.Image), &drawOptions)
+			drawOptions.GeoM.Translate(float64(shot.bulletXLoc), float64(shot.bulletYLoc))
+			screen.DrawImage(shot.sprite, &drawOptions)
 		}
 	}
 
@@ -349,8 +432,10 @@ func main() {
 	animationGuy := LoadEmbeddedImage("", "player.png")
 	enemyAnimation := LoadEmbeddedImage("", "earthEnemy.png")
 	npc1Animation := LoadEmbeddedImage("", "npc2.png")
+	shotAnimation := LoadEmbeddedImage("", "laser.png")
 
 	drawFont := LoadScoreFont()
+	allShots := make([]shots, 0, 20)
 
 	oneLevelGame := AnimatedSpriteDemo3{
 		levels:      0,
@@ -361,7 +446,9 @@ func main() {
 		tileHash:    ebitenImageMap,
 		damage:      1,
 		textFont:    drawFont,
-		enemy: enemy{
+		shot:        allShots,
+		bullet:      shotAnimation,
+		enemy1: enemy{
 			sprite:    enemyAnimation,
 			xLocNnemy: 200,
 			yLocEnemy: 608,
@@ -373,6 +460,13 @@ func main() {
 			xLocNnemy: 570,
 			yLocEnemy: 384,
 			direction: ENEMY_LEFT,
+			alive:     true,
+		},
+		enemy3: enemy{
+			sprite:    enemyAnimation,
+			xLocNnemy: 320,
+			yLocEnemy: 300,
+			direction: ENEMY_UP,
 			alive:     true,
 		},
 		npc1: npc{
